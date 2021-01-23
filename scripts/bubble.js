4e242c7c87edc70a93bf0bda338e61aee65936bf
TheZackMatthews <Zackary.matthews@yahoo.com>
2021-01-23 14:22:13 -0800

    Bubblesort: Minor upgrade in for loop (removed unnessessary iterations)

--- scripts/bubble.js
+++ scripts/bubble.js
@@ -3,12 +3,11 @@
 function bubbleSort (arr) {
   //We pass through the array and swap elements to the right. This means we'll have the largest index at the end of each pass
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
+    for (let j = 0; j < arr.length - i; j++) {
       if (arr[j] > arr[i]) {
         [arr[j], arr[i]] = [arr[i], arr[j]];
       }
     }
   }
 }
-
 module.exports = bubbleSort;
